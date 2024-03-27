e89d26cbb142c284d88adf2023af1e73bb86e834
volcom boy <yamilmsilva@gmail.com>
2024-03-27 14:30:43 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const swiper = new Swiper('.swiper-hero', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    direction: 'horizontal',
+    loop: true,
+    allowTouchMove: true,
+    // autoplay: {
+    //     delay:1000,
+    //     delay:10000,
+    //     pauseOnMouseEnter: true,
+    //     },
+  
+    // If we need pagination
+   
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
